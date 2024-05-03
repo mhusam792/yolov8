4a08e510360d80756a7ffa393059bdf28eb76a06
‪Mohamed Hussam‬‏ <106589725+mhusam792@users.noreply.github.com>
2024-05-03 06:58:43 +0300

    Update api.py

--- api.py
+++ api.py
@@ -7,7 +7,7 @@ from fastapi import HTTPException
 from main_app.image_predictor import ImagePredictor
 
 
-model_path = 'last.pt'
+model_path = '/content/yolov8/last.pt'
 image_predictor = ImagePredictor(model_full_path=model_path)
 
 def read_image_from_url(url: str) -> Image.Image:
